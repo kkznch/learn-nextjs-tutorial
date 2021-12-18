64a79a94f7549f55ef586e6e9ac9f872c44db2ac
kkznch <kkznch710md@gmail.com>
2021-12-18 15:21:51 +0900

    classnamesライブラリ入れてアラート実装してみた

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,16 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
+import Alert from '../../components/Alert'
 
 export default function FirstPost() {
+  const [alertType, setAlertType] = useState('success')
+  const changeAlertType = () => {
+    console.log(alertType)
+    setAlertType(alertType === 'success' ? 'error' : 'success')
+  }
+
   return (
     <Layout>
       <Head>
@@ -14,6 +22,8 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
+      <Alert type={alertType}>アラートです</Alert>
+      <button onClick={changeAlertType}>アラートの色変えるよ</button>
     </Layout>
   )
 }
